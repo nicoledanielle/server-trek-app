43a1ac3552c7fc2f9d63a2a3d5f2278e334f7c23
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-13 10:52:26 -0800

    added mlab database plus files for auth index, router, strategies

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://dev:dev@ds235418.mlab.com:35418/trek-app-proto',
     TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test'
